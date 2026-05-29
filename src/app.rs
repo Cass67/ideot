e7d86720239994c3e0f4b2a545b4fed193a788fa
Cass <867000+Cass67@users.noreply.github.com>
2026-05-29 17:52:48 +0100

    feat: add lsp ready document events

--- src/app.rs
+++ src/app.rs
@@ -1,15 +1,17 @@
 pub mod command;
 
+use crate::lsp::NullDocumentEventSink;
 use std::path::PathBuf;
 
 #[derive(Debug)]
 pub struct App {
     pub root: PathBuf,
     pub should_quit: bool,
+    pub lsp_sink: NullDocumentEventSink,
 }
 
 impl App {
     pub fn new(root: PathBuf) -> Self {
-        Self { root, should_quit: false }
+        Self { root, should_quit: false, lsp_sink: NullDocumentEventSink }
     }
 }
